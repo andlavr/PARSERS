408fc8bdb8d58db3363a6b16720e24780d152caa
vladuxa23 <vlad_1995.02@mail.ru>
2022-04-01 19:38:45 +0300

    refactor

--- settings.py
+++ settings.py
@@ -1,22 +1,8 @@
 """
 Модуль используется для хранения настроек пакета requests и приложения в целом
 """
-import requests
 
-# prox = {'39.106.228.34:8082', '51.77.159.133:80', '50.87.181.51:80', '58.137.62.133:80','82.223.108.75:80',
-# '80.48.119.28:8080','20.105.253.176:8080', '47.111.94.201:1234', '178.73.192.6:8118', '140.246.87.238:3128'}
-# for proxy in prox:
-#     response = requests.get(proxies=proxy)
-#     if response.status_code == requests.codes['ok']:
-#         break
-#
-# # response.text
-
-proxies = {
-    'http': 'http://39.106.228.34:8080',
-    'http': 'http://167.114.96.27:5566'}
-# proxies = {
-#     'http': 'http://185.119.59.244:80'}
+proxies = {'http': 'http://39.106.228.34:8080'}
 
 headers = {
     'content-type': "application/json;charset=utf-8",
